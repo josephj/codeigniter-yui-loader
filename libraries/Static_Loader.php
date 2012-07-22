9eb6d631ed4b2b738dc562697f3f5f6689d13db2
Joseph Chiang <joseph_chiang@miiicasa.com>
2012-07-22 14:42:42 +0800

    Self code reveiw.

--- libraries/Static_Loader.php
+++ libraries/Static_Loader.php
@@ -1,21 +1,22 @@
 <?php
-
 if ( ! defined("BASEPATH"))
 {
     exit("No direct script access allowed");
 }
-
 /**
  * Read the static config to generate inline YUI config.
+ *
+ *    $this->load->library("static_loader");
+ *    $this->static_module->set("common/_masthead", "home/_notification");
+ *    $data["loader_html"] = $this->static_module->load();
  *
  * @class Static_loader
  */
 class Static_Loader {
 
-    private $_static_config;
-    private $_user_modules;
-    private $_css_files;
-    private $_groups;
+    public $yui_config;
+    public $css_files;
+    public $user_modules;
 
     public function __construct()
     {
@@ -24,140 +25,92 @@ class Static_Loader {
     }
 
     /**
-     * Non-recursively collect all required modules.
+     * Get YUI JavaScript module config.
      *
-     * @method _get_require_modules
-     * @private
-     * @param module {Array} The module name list.
-     * @return {Array} The require modules.
-     */
-    private function _get_require_modules($modules)
-    {
-        $config = $this->config->item("static");
-        $require_modules = array();
-        foreach ($modules as $module)
-        {
-            $require_modules[] = $module;
-            $target = $config["modules"][$module];
-            if ( ! isset($target) || ! isset($target["requires"]))
-            {
-                continue;
-            }
-            $require_modules = array_merge($require_modules, $target["requires"]);
-        }
-        return array_unique($require_modules);
-    }
-
-    /**
-     * Recursively collect all required modules based on _get_require_modules.
-     *
-     * @method _get_all_require_modules
-     * @private
-     * @param module {Array} The module name list.
-     * @return {Array} The require modules.
-     */
-    private function _get_all_require_modules($modules)
-    {
-        $current_modules = array();
-        $require_modules = $modules;
-        while ( ! $is_end)
-        {
-            $required_modules = $this->_get_require_modules($require_modules);
-            if ($current_modules === $required_modules)
-            {
-                $is_end = TRUE;
-            }
-            else
-            {
-                $current_modules = $required_modules;
-            }
-        }
-        return $required_modules;
-    }
-
-    /**
-     * Get JavaScript config for a single module.
-     *
-     * @param $module {Array} The original module configuration.
+     * @param $module {Array} The static module configuration.
+     * @return {Array} The YUI JavaScript module config.
      */
     private function _get_js_config($module)
     {
+
+        if ( ! isset($module["js"])) {
+            return FALSE;
+        }
+
+        // Move 'js' attribute to 'path'
+        // to keep align with YUI config.
         $data = array();
-        foreach ($module as $key => $data)
+        if (isset($module["js"]))
         {
-            if (isset($module["js"]))
+            $data["path"] = $module["js"];
+            unset($module["js"]);
+        }
+
+        // List the attributes which shouldn't
+        // be attached.
+        $allows = array("lang", "async", "requires");
+        foreach ($module as $key => $value)
+        {
+            if (in_array($key, $allows))
             {
-                $data = array();
-                if (isset($module["js"]))
-                {
-                    $data["path"] = $module["js"];
-                }
-                if (isset($module["lang"]))
-                {
-                    $data["lang"] = $module["lang"];
-                }
-                if (isset($module["async"]))
-                {
-                    $data["async"] = $module["async"];
-                }
-                if (isset($module["requires"]))
-                {
-                    $data["requires"] = $module["requires"];
-                }
-            }
-            else if (isset($module["css"]))
-            {
-                continue;
-            }
-            else
-            {
-                $data[$key] = $data;
+                $data[$key] = $value;
             }
         }
+        $data["type"] = "js";
         return $data;
     }
 
     /**
-     * Transform our customized configuration (config/static.php)
-     * to YUI config.
+     * Get the loader HTML.
      *
-     * @param $modules {Array} The module name list.
-     * @return {String} The output HTML code.
+     *     echo $this->static_loader->load();
+     *
+     * @return {String} The loader HTML code.
      */
-    private function _get_html($css_files, $groups)
+    public function load()
     {
-        $html   = array();
+        $html       = array();
+        $config     = $this->yui_config;
+        $css_files  = $this->css_files;
+        $modules    = implode("\",\"", $this->user_modules);
+        $tpl_link   = '<link rel="stylesheet" href="combo/?g=css&f=%s">';
+        $tpl_script = array(
+            '<script type="text/javascript" src="combo/?g=js"></script>',
+            '<script type="text/javascript">YUI_config = %s;</script>',
+            '<script>YUI().use("' . $modules . '");</script>',
+        );
+        $tpl_script = implode("\n", $tpl_script);
+
         if (count($css_files))
         {
-            $html[] = '<link rel="stylesheet" href="combo/?g=css&f=' . implode(",", $css_files) . '">';
+            $html[] = sprintf($tpl_link, implode(",", $css_files));
         }
-        $html[] = '<script src="combo/?g=js"></script>';
-        $config = array(
-            "filter"   => "raw",
-            "async"    => FALSE,
-            "combine"  => TRUE,
-            "comboBase"=> "combo/?f=",
-            "comboSep" => ",",
-            "root"     => "lib/yui/build/",
-            "langs"     => "zh-TW,en-US",
-            "groups"   => $groups,
-        );
-        $html[] = "<script>YUI_config = " . json_encode($config) . ";</script>";
-        $html[] = "<script>YUI().use(\"" . implode("\",\"", $this->_user_modules) . "\");</script>";
+        $html[] = sprintf($tpl_script, json_encode($config));
         return implode("\n", $html);
     }
 
-    public function load($modules)
+    /**
+     * Set modules you want use.
+     *
+     *    $this->static_module->set("common/_masthead", "home/_notification");
+     *
+     * @method set
+     * @public
+     */
+    public function set($modules)
     {
-        $groups = array();
-        $css_files = array();
-        $is_end = FALSE;
+        if (gettype(func_get_arg(0)) === "string")
+        {
+            $modules = func_get_args();
+        }
+        $this->user_modules = $modules;
+
+        // Load configuration file - config/static.php.
         $this->config->load("static", TRUE);
-        $this->_user_modules = $modules;
-        $this->_static_config = $this->config->item("static");
         $config = $this->config->item("static");
 
-        // Set group basic data.
+        // Make groups config.
+        $groups = array();
         foreach ($config["groups"] as $name => $data)
         {
             $groups[$name] = array(
@@ -169,9 +122,11 @@ class Static_Loader {
             );
         }
 
-        // Get all required modules.
-        $modules = array_keys($this->_static_config["modules"]);
+        // The CSS files which needs to be combined.
+        $css_files = array();
 
+        // Make 'groups' config ready.
+        $modules = array_keys($config["modules"]);
         foreach ($modules as $module_name)
         {
             // Ignore modules which are not defined in configuration.
@@ -181,71 +136,65 @@ class Static_Loader {
                 continue;
             }
 
-            $module = $config["modules"][$module_name];
+            $module     = $config["modules"][$module_name];
             $group_name = $module["group"];
-            $group = $groups[$group_name];
+            $group      = $groups[$group_name];
 
+            // Attach JavaScript config.
             if (isset($module["js"]))
             {
                 $groups[$group_name]["modules"][$module_name] =
                     $this->_get_js_config($module);
             }
 
-            if (isset($module["css"]))
+            // Stop here and continue to next iteration
+            // if no css attribute exists.
+            if ( ! isset($module["css"]))
             {
-                // Check if belonging group uses CSS server combo.
-                $server_combo = $config["groups"][$group_name]["serverComboCSS"];
+                continue;
+            }
 
-                if (isset($module["js"]))
+            // Check if belonging group uses CSS server combo.
+            $server_combo = !($group["fetchCSS"]);
+            if ($server_combo)
+            {
+                $css_files[] = $group["root"] . $module["css"];
+                if (
+                    ! isset($module["js"]) &&
+                    in_array($module_name, $this->user_modules)
+                )
                 {
-                    if ($server_combo)
-                    {
-                        $css_files[] = $group["root"] . $module["css"];
-                    }
-                    else
-                    {
-                        // Add a new module which related with javascript.
-                        $group["modules"]["$module_name-css"] = array(
-                            "path" => $module["css"],
-                            "type" => "css",
-                        );
-                        $group["modules"][$module_name]["requires"][] = $require_module-css;
-                    }
-                }
-                else
-                {
-                    if ($server_combo)
-                    {
-                        $css_files[] = $group["root"] . $module["css"];
-
-                        if (isset($module["requires"]))
-                        {
-                            $group["modules"][$module_name]["requires"] = $module["requires"];
-                        }
-                        else
-                        {
-                            // Remove this module.
-                            for ($i = count($this->_user_modules) - 1, $j = 0; $i >= $j; $i--)
-                            {
-                                if ($this->user_modules[$i] === $module_name)
-                                {
-                                    array_splice($this->user_modules, $i, 1);
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        $groups[$group_name]["modules"][$module_name] = array(
-                            "path" => $module["css"],
-                            "type" => "css",
-                        );
-                    }
+                    $offset = array_search($module_name, $this->user_modules);
+                    unset($this->user_modules[$offset]);
                 }
             }
+            else
+            {
+                if (isset($module["js"]))
+                {
+                    $group["modules"][$module_name]["requires"][] = "$module_name-css";
+                    $module_name = "$module_name-css";
+                }
+                $groups[$group_name]["modules"][$module_name] = array(
+                    "path" => $module["css"],
+                    "type" => "css",
+                );
+            }
         }
-        return $this->_get_html($css_files, $groups);
 
+        $this->css_files  = $css_files;
+
+        // TODO - Make these attribute configurable.
+        $this->yui_config = array(
+            "filter"    => "raw",
+            "async"     => FALSE,
+            "combine"   => TRUE,
+            "comboBase" => "combo/?f=",
+            "comboSep"  => ",",
+            "root"      => "lib/yui/build/",
+            "langs"     => "zh-TW,en-US",
+            "groups"    => $groups,
+        );
     }
 }
 /* End of file Static_Loader.php */
